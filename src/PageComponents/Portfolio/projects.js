58a3fe03c9eca5bb4dfd42f7138cf25a9c4f1aed
Pranav Vishal <ronvishal77@gmail.com>
2022-10-12 22:23:15 -0400

    Extra GameDev Projects

--- src/PageComponents/Portfolio/projects.js
+++ src/PageComponents/Portfolio/projects.js
@@ -2,6 +2,8 @@ import MotorMayhem from "./MotorMayhem_Portfolio.PNG";
 import RockStars from "./RockStars_Portfolio_Photo.PNG";
 import PersonalSite from "./PersonalSite_Portfolio.PNG";
 import OpenGL from "./openGl.PNG";
+import Pokemon from "./ATLINT.PNG";
+import Platformer from "./image.png";
 
 export const projects = [
   {
@@ -17,6 +19,20 @@ export const projects = [
       "Second mobile game I developed with Unity and C#. Motor Mayhem ranked as a top 100 iOS racing game as of September 2022. I was able to improve the user experience by collecting feedback from a sample of test users in an agile manner, and utilized Plastic SCM to implement version control.",
     image: MotorMayhem,
     url: "https://www.youtube.com/watch?v=2C005k-kujs&ab_channel=PanamaInk",
+  },
+  {
+    title: "All That Lies in the North",
+    description:
+      "Unity2D turn-based rpg, developed for the GameDevTV Game Jam. Implemented turn-based combat system similar to Pokemon, character stats, experience levels, inventory, quest and save system.",
+    image: Pokemon,
+    url: "https://youtu.be/EgBXkn-KGuw",
+  },
+  {
+    title: "BroBot",
+    description:
+      "Unity 3D Platformer made for the University of Waterloo GI Game Jam in which I placed second. Implemented enemy AI, basic melee combat, checkpoints, and a level select system.",
+    image: Platformer,
+    url: "https://www.youtube.com/watch?v=2zQslzGy0Os&t=87s",
   },
   {
     title: "OpenGL 3D Camera",
