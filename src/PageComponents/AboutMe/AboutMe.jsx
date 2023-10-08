6984da8d5740b690cb7d2219dfa03deb0a23dfec
Pranav Vishal <98032263+pranavishal@users.noreply.github.com>
2023-10-08 13:22:59 -0400

    Update AboutMe.jsx

--- src/PageComponents/AboutMe/AboutMe.jsx
+++ src/PageComponents/AboutMe/AboutMe.jsx
@@ -9,7 +9,7 @@ function AboutMe() {
       <div>
         <h1>About Me</h1>
         <p>
-          Hi! I'm a prolific programmer, studying computer science at the
+          Hi! I'm an avid programmer, studying computer science at the
           University of Waterloo. I'm always looking for
           opportunities to increase my skills in new and challenging
           environments. Below you'll find details into my projects, skills, as
